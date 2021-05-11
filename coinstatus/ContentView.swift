72bc8b28c8de15b6804f6235295490c2651fbfd4
yudonghun <donghun15.yu@samsung.com>
2021-05-11 16:29:53 +0900

    Initial Commit

---
+++ coinstatus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coinstatus
+//
+//  Created by 유동훈 on 2021/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coinstatus/coinstatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinstatusApp.swift
+//  coinstatus
+//
+//  Created by 유동훈 on 2021/05/11.
+//
+
+import SwiftUI
+
+@main
+struct coinstatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
